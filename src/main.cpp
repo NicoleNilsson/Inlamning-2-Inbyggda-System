f75eedd4095a30759d89044e354b76d2a5002129
NicoleNilsson <nicole-nilsson@outlook.com>
2024-04-10 14:03:17 +0200

    adc read and write working

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <stdlib.h>
 #include "bit_manipulation.h"
 #include "led.h"
 #include "timer.h"
 #include "serial.h"
+//#include "Arduino.h"
 
 LED redLED(3, DDRD, PORTD); //aka pin 3 on freenove
 
@@ -13,17 +15,36 @@ LED redLED(3, DDRD, PORTD); //aka pin 3 on freenove
 //   redLED.state = !redLED.state;
 // }
 
+void initADC(void){
+  ADMUX = (1 << REFS0); // select ADC0 (pin A0) as input
+  ADCSRA |= (1 << ADEN) | (0 << ADSC) | (0 << ADATE) | (1 << ADPS0)
+  | (1 << ADPS1) | (1 << ADPS2); //ADC enable, auto-trigger OFF, prescaler 128
+  ADCSRB = 0x00; // Free running mode, no power saving
+  // If using interrupts enable here and make sure to have ISR defined!
+}
+
+// Blocking read
+uint16_t readADC(void){
+  // start single conversion
+  ADCSRA |= (1 << ADSC);
+  // wait for conversion to complete, ADSC becomes '0′ again
+  while (ADCSRA & (1 << ADSC));
+  return ADC;
+}
+
 int main(void){
   //setup
-  Timer timer;
   Serial uart(9600);
-  //sei(); //enable interrupts
+  initADC();
+  uint16_t ADCValue = 0;
+  char str[10];
+  
 
   while(1){
-    if(HAS_RECIEVED_CHAR){
-      redLED.LEDSerialControl(uart);
-    }
-    redLED.toggleLED();
+    ADCValue = readADC();
+    itoa(ADCValue, str, 10);
+    uart.transmitString(str);
+    uart.transmitChar('\n');
   }
 
   return 0;
