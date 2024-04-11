e08a9e53b7efc6a00316eeaa5ffabb46a1a8e22e
NicoleNilsson <nicole-nilsson@outlook.com>
2024-04-11 11:41:43 +0200

    implemented compb for blink

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,17 @@
 #include "potentiometer.h"
 
 volatile bool eventHappened = false;
+volatile bool blink = false;
 const uint16_t baudRate = 9600;
+uint16_t blinkSpeed = 0;
 
 ISR(TIMER1_COMPA_vect){
   OCR1A += 62500; //interrupt every 1sec
   eventHappened = true;
+}
+
+ISR(TIMER1_COMPB_vect){
+  blink = true;
 }
 
 int main(void){
@@ -20,6 +26,8 @@ int main(void){
   Serial uart(baudRate);
   LED redLED(3, DDRD, PORTD); //aka pin 3 on freenove
 
+  sei();
+
   while(1){
     if(eventHappened){
       //vill jag hämta adc value innan jag anropar print?
@@ -27,7 +35,23 @@ int main(void){
       potentiometer.printVoltage(uart);
       eventHappened = false;
     }
+    uint16_t ADCValue = potentiometer.readADC();
+    uint8_t voltage = (ADCValue * (5.0 / 1023));
+
+    if(voltage == 5){
+      OCR1B = 25000;
+    }
+
+    if(blink){
+      redLED.state = !redLED.state;
+      blink = false;
+    }
+    redLED.toggleLED();
   }
 
   return 0;
+}
+
+int mapFunction(int value, int fromLow, int fromHigh, int toLow, int toHigh) {
+  return (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
 }
--- src/timer.cpp
+++ src/timer.cpp
@@ -6,5 +6,7 @@ void Timer::timerSetup(void){
     TCCR1B = 0; //initiate Timer1
     TCCR1B |= (1 << CS12); //prescaler = 256 
     OCR1A = 62500; //for 1s interval
+    OCR1B = 62500;
     TIMSK1 |= (1 << OCIE1A); //enable Timer COMPA Interrupt
+    TIMSK1 |= (1 << OCIE1B); //enable Timer COMPB Interrupt
 }
